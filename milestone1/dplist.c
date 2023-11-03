fa53e3fb191a5df66015402ea59bb3e8c9a9c22f
Robbe Decapmaker <debber@dcpm.be>
2023-11-03 13:49:43 +0100

    Implement get reference + insert at index

--- milestone1/dplist.c
+++ milestone1/dplist.c
@@ -7,9 +7,6 @@
 #include <assert.h>
 #include "dplist.h"
 
-
-
-
 /*
  * The real definition of struct list / struct node
  */
@@ -70,8 +67,49 @@ void dpl_free(dplist_t **list, bool free_element) {
 }
 
 dplist_t *dpl_insert_at_index(dplist_t *list, void *element, int index, bool insert_copy) {
+  // Stop if the list is NULL
+  if(list == NULL){
+    return NULL;
+  }
 
-    //TODO: add your code here
+  // malloc memory
+  dplist_node_t *list_node;
+  list_node = malloc(sizeof(dplist_node_t));
+
+  // insert the element into the struct
+  if (insert_copy) {
+    list_node->element = list->element_copy(element);
+  }else{
+    list_node->element = element;
+  }
+
+  list_node->element = element;
+  if (list->head == NULL) { // covers case 1
+    list_node->prev = NULL;
+    list_node->next = NULL;
+    list->head = list_node;
+  } else if (index <= 0) { // covers case 2
+    list_node->prev = NULL;
+    list_node->next = list->head;
+    list->head->prev = list_node;
+    list->head = list_node;
+  } else {
+    dplist_node_t *ref_at_index;
+    ref_at_index = dpl_get_reference_at_index(list, index);
+    assert(ref_at_index != NULL);
+    if (index < dpl_size(list)) { // covers case 4
+      list_node->prev = ref_at_index->prev;
+      list_node->next = ref_at_index;
+      ref_at_index->prev->next = list_node;
+      ref_at_index->prev = list_node;
+    } else { // covers case 3
+      assert(ref_at_index->next == NULL);
+      list_node->next = NULL;
+      list_node->prev = ref_at_index;
+      ref_at_index->next = list_node;
+    }
+  }
+  return list;
 
 }
 
@@ -158,9 +196,38 @@ int dpl_get_index_of_element(dplist_t *list, void *element) {
 }
 
 dplist_node_t *dpl_get_reference_at_index(dplist_t *list, int index) {
+  // If we receive a NULL pointer, return NULL
+  if (list == NULL) {
+    return NULL;
+  }
 
-    //TODO: add your code here
+  // First get the length of the dpl
+  int length = dpl_size(list);
 
+  // If there are no elements, return NULL
+  if (length == 0){ 
+    return NULL;
+  }
+
+  // When we want to lookup a non-positive integer index
+  if (index <= 0) {
+    return list->head;
+  }
+
+  // When we want to lookup an index which is larger then the count,
+  // we want to return the last element of the pdl
+  // We can do this by changing the index to be the last possible index
+  if (length <= index){
+    index = length-1;
+  }
+
+  // Get the reference at `index`
+  dplist_node_t *toReturn = list->head;
+  for (int i = 0; i <= index-1; i++) {
+    toReturn = toReturn->next;
+  }
+
+  return toReturn;
 }
 
 void *dpl_get_element_at_reference(dplist_t *list, dplist_node_t *reference) {
