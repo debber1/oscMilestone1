a016323130aa16ea2f375f7187fbe43c08642eb7
Robbe Decapmaker <debber@dcpm.be>
2023-11-03 14:12:09 +0100

    Add test numbers

--- milestone1/ourtest.c
+++ milestone1/ourtest.c
@@ -59,22 +59,22 @@ void test1()
     // Test free NULL, don't use callback
     dplist_t *list = NULL;
     dpl_free(&list, false);
-    ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
+    ck_assert_msg(list == NULL, "Failure: expected result to be NULL (1.1)");
 
     // Test free NULL, use callback
     list = NULL;
     dpl_free(&list, true);
-    ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
+    ck_assert_msg(list == NULL, "Failure: expected result to be NULL (1.2)");
 
     // Test free empty list, don't use callback
     list = dpl_create(element_copy, element_free, element_compare);
     dpl_free(&list, false);
-    ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
+    ck_assert_msg(list == NULL, "Failure: expected result to be NULL (1.3)");
 
     // Test free empty list, use callback
     list = dpl_create(element_copy, element_free, element_compare);
     dpl_free(&list, true);
-    ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
+    ck_assert_msg(list == NULL, "Failure: expected result to be NULL (1.4)");
 
 
     my_element_t *content = (my_element_t *)malloc(sizeof(my_element_t));
@@ -84,12 +84,12 @@ void test1()
     list = dpl_create(element_copy, element_free, element_compare);
     dpl_insert_at_index(list, content, 0, false);
     dpl_free(&list, false);
-    ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
+    ck_assert_msg(list == NULL, "Failure: expected result to be NULL (1.5)");
 
     list = dpl_create(element_copy, element_free, element_compare);
     dpl_insert_at_index(list, content, 0, true);
     dpl_free(&list, true);
-    ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
+    ck_assert_msg(list == NULL, "Failure: expected result to be NULL (1.6)");
 
 
     char *test2 = "123";
@@ -101,7 +101,7 @@ void test1()
     dpl_insert_at_index(list, content2, 0, false);
     dpl_insert_at_index(list, content, 99, false);
     dpl_free(&list, false);
-    ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
+    ck_assert_msg(list == NULL, "Failure: expected result to be NULL (1.7)");
 
 
     list = dpl_create(element_copy, element_free, element_compare);
@@ -109,7 +109,7 @@ void test1()
     dpl_insert_at_index(list, content, 0, true);
     dpl_insert_at_index(list, content2, 99, true);
     dpl_free(&list, true);
-    ck_assert_msg(list == NULL, "Failure: expected result to be NULL");
+    ck_assert_msg(list == NULL, "Failure: expected result to be NULL (1.8)");
 
     free(content);
     free(content2);
@@ -126,10 +126,10 @@ void test2()
 
   
     list = dpl_insert_at_index(NULL, content, -1, false);
-    ck_assert_msg(list == NULL, "Failure: expected list to be NULL");
+    ck_assert_msg(list == NULL, "Failure: expected list to be NULL (2.1)");
 
     list = dpl_insert_at_index(NULL, content, 99, true);
-    ck_assert_msg(list == NULL, "Failure: expected list to be NULL");
+    ck_assert_msg(list == NULL, "Failure: expected list to be NULL (2.2)");
 
     free(content);
 }
@@ -164,36 +164,36 @@ void test3()
 
 
 
-    ck_assert_msg(dpl_size(list) == 3, "Failure: expected list to have size of 3");
+    ck_assert_msg(dpl_size(list) == 3, "Failure: expected list to have size of 3 (3.1)");
 
     dplist_node_t *node = (dplist_node_t *)dpl_get_reference_at_index(list, -1);
     my_element_t *el = (my_element_t *)dpl_get_element_at_reference(list, node);
 
-    ck_assert_msg(node != NULL, "Failure: expected node at -1 is NULL");
-    ck_assert_msg(el != NULL, "Failure: expected element at -1 is NULL");
+    ck_assert_msg(node != NULL, "Failure: expected node at -1 is NULL (3.2)");
+    ck_assert_msg(el != NULL, "Failure: expected element at -1 is NULL (3.3)");
 
 
-    ck_assert_msg(element_compare(el, (void *)content1) == 0, "Failure: expected element at -1 is not content2");
+    ck_assert_msg(element_compare(el, (void *)content1) == 0, "Failure: expected element at -1 is not content2 (3.4)");
 
     node = (dplist_node_t *)dpl_get_reference_at_index(list, 99);
     el = (my_element_t *)dpl_get_element_at_reference(list, node);
 
-    ck_assert_msg(node != NULL, "Failure: expected node at 99 is NULL");
-    ck_assert_msg(el != NULL, "Failure: expected element at 99 is NULL");
+    ck_assert_msg(node != NULL, "Failure: expected node at 99 is NULL (3.5)");
+    ck_assert_msg(el != NULL, "Failure: expected element at 99 is NULL (3.6)");
 
 
-    ck_assert_msg(element_compare(el, (void *)content3) == 0, "Failure: expected element at 99 is not content3");
+    ck_assert_msg(element_compare(el, (void *)content3) == 0, "Failure: expected element at 99 is not content3 (3.7)");
 
      list = dpl_remove_at_index(list, -2, false);
 
-     ck_assert_msg(dpl_size(list) == 2, "Failure: expected list to have size of 2");
+     ck_assert_msg(dpl_size(list) == 2, "Failure: expected list to have size of 2 (3.8)");
 
      list = dpl_remove_at_index(list, 98, false);
 
-     ck_assert_msg(dpl_size(list) == 1, "Failure: expected list to have size of 1");
+     ck_assert_msg(dpl_size(list) == 1, "Failure: expected list to have size of 1 (3.9)");
 
     dpl_free(&list, false);
-    ck_assert_msg(list == NULL, "Failure: expected list to be NULL");
+    ck_assert_msg(list == NULL, "Failure: expected list to be NULL (3.10)");
 
 
 
@@ -206,20 +206,20 @@ void test3()
 
     dpl_insert_at_index(list, content2, 1, true);
 
-    ck_assert_msg(dpl_size(list) == 3, "Failure: expected list to have size of 3");
+    ck_assert_msg(dpl_size(list) == 3, "Failure: expected list to have size of 3 (3.11)");
 
                 
 
     list = dpl_remove_at_index(list, -2, true);
 
-    ck_assert_msg(dpl_size(list) == 2, "Failure: expected list to have size of 2");
+    ck_assert_msg(dpl_size(list) == 2, "Failure: expected list to have size of 2 (3.12)");
 
     list = dpl_remove_at_index(list, 98, true);
 
-    ck_assert_msg(dpl_size(list) == 1, "Failure: expected list to have size of 1");
+    ck_assert_msg(dpl_size(list) == 1, "Failure: expected list to have size of 1 (3.13)");
 
     dpl_free(&list, true);
-    ck_assert_msg(list == NULL, "Failure: expected list to be NULL");
+    ck_assert_msg(list == NULL, "Failure: expected list to be NULL (3.14)");
 
     free(content1);
     free(content2);
